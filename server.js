5344b49c3ca7ec3957532b7b62d41f5a32606cc3
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2020-10-08 15:14:46 +0100

    third commit

--- server.js
+++ server.js
@@ -1,43 +1,50 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./stagethree/usermodel");
 // initializing express
 const app = express();
 
-const users = [
-  { id: 1, name: "user1" },
-  { id: 2, name: "user2" },
-  { id: 3, name: "user3" },
-  { id: 4, name: "user4" },
-  { id: 5, name: "user5" },
-  { id: 6, name: "user6" },
-  { id: 7, name: "user7" },
-  { id: 8, name: "user8" },
-  { id: 9, name: "user9" },
-  { id: 10, name: "user10" },
-];
+// connecting to database
+mongoose
+  .connect("mongodb://localhost/pagination", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    console.log("database is connected to mongodb://localhost/pagination")
+  )
+  .catch(() => console.log("error connecting to database"));
 
-const posts = [
-  { id: 1, name: "post1" },
-  { id: 2, name: "post2" },
-  { id: 3, name: "post3" },
-  { id: 4, name: "post4" },
-  { id: 5, name: "post5" },
-  { id: 6, name: "post6" },
-  { id: 7, name: "post7" },
-  { id: 8, name: "post8" },
-  { id: 9, name: "post9" },
-  { id: 10, name: "post10" },
-];
+//   initializing db to mongoose connectin method
+const db = mongoose.connection;
 
-app.get("/posts", paginate(posts), (req, res) => {
-  res.json(res.paginatedResult);
+// inserting data to the user collection database
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) > 0)
+    return console.log("user already inserted to database");
+
+  Promise.all([
+    User.create({ name: "user1" }),
+    User.create({ name: "user2" }),
+    User.create({ name: "user3" }),
+    User.create({ name: "user4" }),
+    User.create({ name: "user5" }),
+    User.create({ name: "user6" }),
+    User.create({ name: "user7" }),
+    User.create({ name: "user8" }),
+    User.create({ name: "user9" }),
+    User.create({ name: "user10" }),
+  ])
+    .then(() => console.log("Added users to database"))
+    .catch(() => console.log("error occured while inserting data to database"));
 });
 
-app.get("/users", paginate(users), (req, res) => {
+app.get("/users", paginate(User), (req, res) => {
   res.json(res.paginatedResult);
 });
 
 function paginate(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
     const startIndex = (page - 1) * limit;
@@ -45,7 +52,7 @@ function paginate(model) {
 
     const result = {};
 
-    if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments().exec())) {
       result.next = {
         page: page + 1,
         limit: limit,
@@ -57,9 +64,14 @@ function paginate(model) {
         limit: limit,
       };
     }
-    result.results = model.slice(startIndex, endIndex);
-    res.paginatedResult = result;
-    next();
+    try {
+      result.results = await model.find().limit(limit).skip(startIndex);
+      res.paginatedResult = result;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+    // result.results = model.slice(startIndex, endIndex);
   };
 }
 // creating a port for server to listen on
